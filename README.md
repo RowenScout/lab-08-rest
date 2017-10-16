cf08ce61d508c614cc2a89ea01bc380eb333894b
Pmookerjee <pmookerjee@gmail.com>
2017-10-15 23:56:29 -0700

    Updated readme and tests

--- README.md
+++ README.md
@@ -1,57 +1,24 @@
-![cf](https://i.imgur.com/7v5ASc8.png) Lab 08: Vanilla REST API
-======
+*_server*
 
-## Submission Instructions
-  * fork this repository & create a new branch for your work
-  * write all of your code in a directory named `lab-` + `<your name>` **e.g.** `lab-susan`
-  * push to your repository
-  * submit a pull request to this repository
-  * submit a link to your PR in canvas
-  * write a question and observation on canvas
+  /api/notes
 
-## Learning Objectives  
-* students will learn to use promise constructs to manage asynchronous code
-* students will learn to create a vanilla RESTful API with in-memory persistence
+  Returns _404_ for a non-existent uri
+  Returns _400_ for bad json data or bad request query
 
-## Requirements
-#### Configuration
-  * `.gitignore`
-  * `.eslintrc`
-  * `package.json`
-  * `README.md`
+  _POST_ creates new note if title and content json is sent in request
+  returns 400 if bad or incomplete json is sent
 
-#### Feature Tasks
-* create the following directories to organize your code:
-  * `lib`
-  * `model`
-  * `test`
-* create an HTTP server using the native NodeJS `http` module
-* create an object constructor that creates a _simple resource_ with at least 3 properties
-  * include an `id` property that is set to a unique id (**hint:** you'll need to use `node-uuid`)
-  * include two additional properties of your choice (ex: name, content, etc.)
-* create a custom body parser module that uses promises to parse the JSON body of `POST` and `PUT` requests
-* create a custom url parser module that returns a promise and uses the NodeJS `url` and `querystring` modules to parse the request url
-* create a router constructor that handles requests to `GET`, `POST`, `PUT`, and `DELETE` requests
-* create a storage module that will store resources by their schema type (ex: note) and id
+  _GET_ if an id is specified, will return the note corresponding to that id.
+  IF no id is specified, will return all notes
 
-## Server Endpoints
-### `/api/simple-resource-name`
-* `POST` request
- * pass data as stringifed JSON in the body of a **POST** request to create a new resource
-* `GET` request
- * pass `?id=<uuid>` as a query string parameter to retrieve a specific resource (as JSON)
-* `DELETE` request
- * pass `?id=<uuid>` in the query string to **DELETE** a specific resource
- * this should return a 204 status code with no content in the body
+  _PUT_ requires an id specified in request query. If no id, will send 400. If id is there, but no
+  title and content, will send 400.
+  Will remove the current note entry from array, and replace it with the contents of the request body.
 
-## Tests
-* write a test to ensure that your api returns a status code of 404 for routes that have not been registered
-* write tests to ensure the `/api/simple-resource-name` endpoint responds as described for each condition below:
- * `GET`: test 404, it should respond with 'not found' for valid requests made with an id that was not found
- * `GET`: test 400, it should respond with 'bad request' if no id was provided in the request
- * `GET`: test 200, it should contain a response body for a request made with a valid id
- * `POST`: test 400, it should respond with 'bad request' if no request body was provided or the body was invalid
- * `POST`: test 200, it should respond with the body content for a post request with a valid body
+  _PATCH_ requires an id specified in request query. If no id, will send 400. If id is there, but no
+  title or content, will send 400. Will update existing entry with the provided data.
 
-## Bonus
-* **2pts:** a `GET` request to `/api/simple-resource-name` with no **?id=** should return an array of all of the ids for that resource
+  _DELETE_ requires an id specified in request query. Will remove the corresponding entry from the Notes array. Returns a 204 No Content
+
+  *tests*
+  run with `mocha`
--- test/note-routes.test.js
+++ test/note-routes.test.js
@@ -1,55 +1,197 @@
-/*global beforeAll,afterAll,expect*/
 'use strict';
 
 process.env.PORT = 4000;
-const server = require('../lib/_server');
-const superagent = require('superagent');
+const request = require('superagent');
+const expect = require('expect');
 
-describe("api/notes", function() {
+describe('api/notes', function() {
 
-  beforeAll(server.start);
-  afterAll(server.stop);
+  let noteID = '';
 
-  describe("POST /api/notes", () => {
+  before((done) => {
+    require('../lib/_server').start();
+    done();
+  });
+  after((done) => {
+    require('../lib/_server').stop();
+    done();
+  });
 
-    test('should respond with a 200', () =>{
-      return superagent.post('http://localhost:5500/api/notes')
-      .set("Content-Type", "application/json")
+
+  describe('POST /api/notes', () => {
+
+    it('should respond with a 200', (done) => {
+
+      request
+      .post('http://localhost:4000/api/notes')
+      .set('Content-Type', 'application/json')
       .send({
-        title:"hello world",
-        content: "this is my first note"
+        title: 'Note1',
+        content: 'This is my first note',
       })
-      .then(res=>{
+      .then(res => {
         expect(res.status).toEqual(200);
-        expect(res.body.title).toEqual('hello world');
-        expect(res.body.content).toEqual('this is my first note');
-      })
+        expect(res.body.title).toEqual('Note1');
+        expect(res.body.content).toEqual('This is my first note');
+        noteID = res.body.id;
+      });
+      done();
     });
 
-    test('should respond with a 400', () =>{
-      return superagent.post('http://localhost:5500/api/notes')
-      .set("Content-Type", "application/json")
+    it('should respond with a 200', () => {
+
+      return request
+      .post('http://localhost:4000/api/notes')
+      .set('Content-Type', 'application/json')
       .send({
-        content: "this is my first note"
+        title: 'Note2',
+        content: 'This is my second note',
       })
-      .then(Promise.reject)
-      .catch(res=>{
-        expect(res.status).toEqual(400);
-      })
+      .then(res => {
+        expect(res.status).toEqual(200);
+        expect(res.body.title).toEqual('Note2');
+        expect(res.body.content).toEqual('This is my second note');
+        noteID = res.body.id;
+      });
+
     });
 
-    test('should respond with a 400', () =>{
-      return superagent.post('http://localhost:5500/api/notes')
-      .set("Content-Type", "application/json")
+    it('should respond with a 400 and "Missing Title"', (done) => {
+
+      request
+      .post('http://localhost:4000/api/notes')
+      .set('Content-Type;', 'application/json')
       .send({
-        title: "my title"
+        content: 'This is my first note',
       })
-      .then(Promise.reject)
-      .catch(res=>{
+      .then(res => {
         expect(res.status).toEqual(400);
+        expect(res.body).toEqual('Missing Title');
+      });
+      done();
+    });
+
+    it('should respond with a 400 and "Missing Content"', (done) =>{
+
+      request
+      .post('http://localhost:5500/api/notes')
+      .set('Content-Type', 'application/json')
+      .send({
+        title: 'Some Fabulous Title',
       })
+      .then(res => {
+        expect(res.status).toEqual(400);
+        expect(res.body).toEqual('Missing Content');
+      });
+      done();
     });
 
   });
 
+  describe('GET /api/notes', () => {
+
+    it('should return a 404 for an unregistered route', (done) => {
+
+      request
+      .get('http://localhost:4000/api/goats')
+      .then(res => {
+        expect(res.status).toEqual(404);
+        expect(res.body).toEqual('Page Not Found');
+      });
+      done();
+
+    });
+
+
+    it('should return all notes if no id is specified', (done) => {
+
+      request
+      .get(`http://localhost:4000/api/notes`)
+      .then(res => {
+        expect(res.status).toEqual(200);
+        expect(res.body).not.toBe(null);
+      });
+      done();
+    });
+
+    it('should return a 200 for a valid note id', (done) => {
+
+      request
+      .get(`http://localhost:4000/api/notes?=${noteID}`)
+      .then(res => {
+        expect(res.status).toEqual(200);
+      });
+      done();
+    });
+
+  });
+
+  describe('PUT /api/notes', () => {
+
+    it('should respond with a 200 and update the note', (done) => {
+
+      request
+      .post(`http://localhost:4000/api/notes?id=${noteID}`)
+      .set('Content-Type', 'application/json')
+      .send({
+        title: 'Note11',
+        content: 'This is my eleventh note',
+      })
+      .then(res => {
+        expect(res.status).toEqual(200);
+        expect(res.body.title).toEqual('Note11');
+        expect(res.body.content).toEqual('This is my eleventh note');
+      });
+      done();
+    });
+
+  describe('PATCH /api/notes', () => {
+
+    it('should respond with a 200 and update the note content only', (done) => {
+
+      request
+      .post(`http://localhost:4000/api/notes?id=${noteID}`)
+      .set('Content-Type', 'application/json')
+      .send({
+        content: 'WOOHOO',
+      })
+      .then(res => {
+        expect(res.status).toEqual(200);
+        expect(res.body.title).toEqual('Note11'); //didn't change the title
+        expect(res.body.content).toEqual('WOOHOO'); //changed the content
+      });
+      done();
+    });
+
+    it('should respond with a 200 and update the note title only', (done) => {
+
+      request
+      .post(`http://localhost:4000/api/notes?id=${noteID}`)
+      .set('Content-Type', 'application/json')
+      .send({
+        title: 'Shenanigans',
+      })
+      .then(res => {
+        expect(res.status).toEqual(200);
+        expect(res.body.title).toEqual('Shenanigans'); //changed the title
+        expect(res.body.content).toEqual('WOOHOO'); //didn't change the content
+      });
+      done();
+    });
+  });
+
+  describe('DELETE /api/notes', () => {
+
+    it('should respond with a 204 and delete the specified note', (done) => {
+
+      request
+      .post(`http://localhost:4000/api/notes?id=${noteID}`)
+      .then(res => {
+        expect(res.status).toEqual(204);
+        expect(res.message).toEqual('No Content');
+      });
+      done();
+    });
+  });
+  });
 });
